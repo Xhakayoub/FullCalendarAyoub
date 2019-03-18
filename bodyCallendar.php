fb6fa7bce45e245fc1e2b961eca701d0dd711cdb
Xhakayoub <43969272+Xhakayoub@users.noreply.github.com>
2019-03-18 09:48:43 +0100

    Update bodyCallendar.php

--- bodyCallendar.php
+++ bodyCallendar.php
@@ -1,64 +1,11 @@
 
 
+<link rel="stylesheet" href="csscCalendar.css" >
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<style>
 
-
-table { 
-	width: 100%; 
-	border-collapse: collapse; 
-	margin:50px auto;
-    table-layout: auto;
-    
-    
-	}
-
-/* Zebra striping */
-tr:nth-of-type(odd) { 
-	background: #eee; 
-	}
-
-th { 
-	background: black; 
-	color: white; 
-	font-weight: bold; 
-    font-size: 12px;
-	}
-
-td, th { 
-	padding: 2px; 
-	border: 1px solid #ccc; 
-	text-align: left;  
-    width: 200px;
-    overflow: hidden;
-
-    white-space: nowrap;
-	}
-
-td{
-        font-size: 9px;
-       
-    }
-.small{
-        width:2%;
-    }
-
-    
-.caseBloque{
-        
-        width:20px;
-        
-    }
-.col-sm-3{
-        height:18px;
-        width:10px;
-        
-    }
-}
-</style>
 <?php
 
 /**
